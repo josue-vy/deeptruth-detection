33c7e1b643abb2a7294c4fe9c03643f9a556f783
josue-vy <josvillanuevay@upt.pe>
2024-06-25 22:25:12 -0500

    deeptruh-detection v0.4.4

--- src/components/api/deteccion.api.ts
+++ src/components/api/deteccion.api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://161.132.39.242/api/face/consult';
+const API_URL = ' https://dissclosure.sytes.net/api/face/consult';
 
 export const consultFace = async (file: File): Promise<{ label: string, probability: number }> => {
     const formData = new FormData();
